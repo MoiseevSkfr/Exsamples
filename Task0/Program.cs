83693fdfab9d0c0c12ccadf141248718b6890e67
Sergey Moiseev <skifride@gmail.comm>
2023-05-18 00:14:05 +0300

    решение задачи1

---
+++ Task0/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+// Например:
+//4 -> 16 
+//-3 -> 9 
+//-7 -> 49
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(number * number);
+Console.WriteLine("квадрат числа равен = " + number * number);
