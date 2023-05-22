cd001928f911cfb24be1ab2bab0ffa482283b432
Sergey Moiseev <skifride@gmail.comm>
2023-05-22 12:18:59 +0300

    add IntroMethod

---
+++ Exsample008_IntroMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿int max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > rusult) result = arg2;
+    if(arg3 > rusult) result = arg3;
+    return result;
+}
+
+
+int a1 = 15;
+int a2 = 21;
+int a3 = 5;
+int b1 = 12;
+int b2 = 23; 
+int b3 = 33;
+int c1 = 13;
+int c2 = 23;
+int c3 = 333;
+
+int max1 = max(a1, b1, c1);
+
+Console.WriteLine(max1);
