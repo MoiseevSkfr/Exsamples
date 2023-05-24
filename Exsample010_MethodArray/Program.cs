5b9fd45de03605851e30859ad85cd59b02e0c61e
Sergey Moiseev <skifride@gmail.comm>
2023-05-24 14:47:57 +0300

    add Exsample010

---
+++ Exsample010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1,12,31,4,15,15,10,30,28};
+
+int n = array.Length;
+int find = 15;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++; //index = index +1;
+}
