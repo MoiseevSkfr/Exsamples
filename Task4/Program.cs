058ace229efabe57dd6dbd9950529de09c5254cb
Sergey Moiseev <skifride@gmail.comm>
2023-05-25 15:22:22 +0300

    add Task4_Method2

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,10 +6,19 @@
 //12->2
 //85-8
 
-void FindLargestDigit(int num)
+int FindFirstDigit(int num)
 {
-    int firstNumber = num / 10; // деление на цело 78 = 7
-    int secondNumber = num % 10; // вычислние последней цыфры 78 = 8
+    int firstNumber = num / 10;
+    return firstNumber;
+}
+int FindSecondDigit(int num)
+{
+    int secondNumber = num % 10;
+    return secondNumber;
+}
+
+void FindLargestDigit(int num,int firstNumber,int secondNumber)
+{
     Console.WriteLine("Рандомное число:" + num);
     Console.WriteLine(firstNumber);
     Console.WriteLine(secondNumber);
@@ -28,4 +37,17 @@ void FindLargestDigit(int num)
     }
 }
 int number = new Random().Next(10, 100);
-FindLargestDigit(number);
+int firstDigit = FindFirstDigit(number);
+int secondDigit = FindSecondDigit(number);
+FindLargestDigit(number,firstDigit,secondDigit);
+
+int number1 = new Random().Next(100);
+int firstDigit1 = FindFirstDigit(number1);
+int secondDigit1 = FindSecondDigit(number1);
+FindLargestDigit(number1, firstDigit1, secondDigit1);
+
+int number2 = new Random().Next(-80, 10);
+
+//FindLargestDigit(number);
+//FindLargestDigit(number1);
+//FindLargestDigit(number2);
