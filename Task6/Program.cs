92f4d060e1254df900044e62f1f79a2b37645d06
Sergey Moiseev <skifride@gmail.comm>
2023-05-26 15:21:20 +0300

    add task6

---
+++ Task6/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+//34, 5 -> не кратно, остаток 4
+//16, 4 -> кратно
+
+Console.WriteLine("Введите число 1:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int number1 = int.Parse(Console.ReadLine());
+FindingMultiple(number, number1);
+
+void FindingMultiple(int num,int num1)
+{
+    int result = num % 10;
+    if (num % num1 == 0)
+    {
+        Console.WriteLine("Второе число кратно первому");
+    }
+    if (num % num1 != 0)
+    {
+        Console.WriteLine("Второе число не кратно первому, остаток " + result);
+    }
+}
+
+
